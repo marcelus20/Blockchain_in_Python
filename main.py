1a1fec072053ef9f5707f9c8a39385e9e854ce20
felipe <marcelus20felipe@gmail.com>
2018-05-23 20:45:09 +0100

    created the transaction class

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+
+"""
+AUTHOR: FELIPE MANTOVANI
+DATE: 23/5/2018
+PROJECT: RUDMENTARY BLOCKCHAIN IN PYTHON
+"""
+
+
 #importation of the library
 import hashlib
 import datetime
+import json
 
 
 
@@ -13,4 +22,29 @@ def encrypt(*args):
     for parameter in args:
         string += str(parameter).encode()
     return hashlib.sha256(string).hexdigest()
+
+
+"""
+The Transaction will hold the sender, receiver and the ammount transfered
+the attributes will remain private and constants
+"""
+class Transaction:
+    def __init__(self, SENDER_ADDRESS, RECEIVER_ADDRESS, AMMOUNT):
+        self.__SENDER_ADDRESS = SENDER_ADDRESS
+        self.__RECEIVER_ADDRESS = RECEIVER_ADDRESS
+        self.__AMOUNT = AMMOUNT
+
+        def getSenderAddress():
+            return self.__SENDER_ADDRESS
+
+        def getReceiverAddress():
+            return self.__RECEIVER_ADDRESS
+        def getAmount():
+            return self.__AMOUNT
+
+
+    def __str__(self):
+        return json.dumps({"sender": self.__SENDER_ADDRESS, "receiver": self.__RECEIVER_ADDRESS, "amount": self.__AMOUNT},
+                          separators=(",", ":"))
+
 
